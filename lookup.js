607ef74a2f7e4c81bd075b3e3d2de346c024485a
crued <crued@10.25.8.157>
2014-09-29 22:27:03 -0400

    Updated to use bootstrap and style results

--- lookup.js
+++ lookup.js
@@ -3,16 +3,21 @@ function getStandardizedName() {
     var compName = document.getElementById("cn").value;
     var Url = "http://api.xsb.com/company-name-standardizer/api/name/" + compName;
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = ProcessRequest;
+    xmlHttp.onreadystatechange = createCallback(xmlHttp, compName);
     xmlHttp.open("GET", Url, true);
     xmlHttp.send(null);
 }
-function ProcessRequest() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        $('#resultList').append('<br/>' + xmlHttp.responseText + '<br/>');
-    } else {
-        if (xmlHttp.readyState == 4) {
-            $('#resultList').append('<br/> Error: Request status - ' + xmlHttp.status + '<br/>');
+
+function createCallback(xhr, compName) {
+    return function ProcessRequest() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                $('#resultList')
+                        .prepend('<div class="bg-info" style="display:none">' + compName + " &rarr; " + xhr.responseText + '</div>');
+                $('#resultList > div').show(250);
+            } else {
+                $('#resultList').prepend('<div class="bg-danger"> Error: Request status - ' + xhr.status + '</div>');
+            }
         }
-    }
+    };
 }
